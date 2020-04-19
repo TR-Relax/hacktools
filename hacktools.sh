5bf71db8ed9a7da620f0c1ddc2ba8d19651ac31a
TR-Relax <63736122+TR-Relax@users.noreply.github.com>
2020-04-19 21:02:20 +0300

    Add files via upload

---
+++ hacktools.sh
@@ -0,0 +1,147 @@
+clear
+echo -e '\e[32m░█░█░█▀█░█▀▀░█░█░░░▀█▀░█▀█░█▀█░█░░░█▀▀
+░█▀█░█▀█░█░░░█▀▄░░░░█░░█░█░█░█░█░░░▀▀█
+░▀░▀░▀░▀░▀▀▀░▀░▀░░░░▀░░▀▀▀░▀▀▀░▀▀▀░▀▀▀'
+echo -e '\e[35m Türkiyem Siber Tim'
+echo -e '\e[32m Kodlayan By \e[31mRelax'
+sleep 2.0
+echo -e '\e[31m[\e[37m01\e[31m]\e[37m Bomb-Botnet'
+sleep 1.0
+echo -e '\e[31m[\e[37m02\e[31m]\e[37m TBomb'
+sleep 1.0
+echo -e '\e[31m[\e[37m03\e[31m]\e[37m BlackEye'
+sleep 1.0
+echo -e '\e[31m[\e[37m04\e[31m]\e[37m XerXes'
+sleep 1.0
+echo -e '\e[31m[\e[37m05\e[31m]\e[37m Hammer'
+sleep 1.0
+echo -e '\e[31m[\e[37m06\e[31m]\e[37m Hulk'
+sleep 1.0
+echo -e '\e[31m[\e[37m07\e[31m]\e[37m Shellphish'
+sleep 1.0
+echo -e '\e[31m[\e[37m08\e[31m]\e[37m Lazymux'
+sleep 1.0
+echo -e '\e[31m[\e[37m09\e[31m]\e[37m RED HAWK'
+sleep 1.0
+echo -e '\e[31m[\e[37m10\e[31m]\e[37m SqlMap'
+sleep 1.0
+read -p $'\e[36mİşlemi Seçiniz\e[35m → \e[37m' secim
+if [[ $secim == 01 || $secim == 1 ]]; then
+echo -e '\e[36m KURULUM BAŞLADI\e[37m'
+apt-get update
+apt-get upgrade
+apt-get install python2
+apt-get install git
+git clone https://github.com/nishinosukasa/Bomb-Botnet
+sleep 3.0
+echo -e '\e[32mKURULUM TAMAMLANDI ÇIKIŞ YAPARAK ÇALIŞTIRABİLİRSİNİZ'
+echo -e '\e[35m 10 Saniye Sonra Ana Menüye Yönlendiriliyorsunuz.'
+sleep 5.0
+bash hacktools.sh
+else
+echo -e '\e[31mHATALI GİRİŞ YAPTINIZ\e[37m'
+fi
+sleep 1.5
+bash hacktools.sh
+if [[ $secim == 02 || $secim == 2 ]]; then
+echo -e '\e[36m KURULUM BAŞLADI\e[37m'
+pkg install git
+pkg install python2
+pip2 install requests git clone
+apt install git
+git clone https://github.com/TheSpeedX/TBomb.git
+echo -e '\e[32mKURULUM TAMAMLANDI ÇIKIŞ YAPARAK ÇALIŞTIRABİLİRSİNİZ'
+echo -e '\e[35m 10 Saniye Sonra Ana Menüye Yönlendiriliyorsunuz.'
+sleep 5.0
+bash hacktools.sh
+else
+if [[ $secim == 03 || $secim == 3 ]]; then
+echo -e '\e[36m KURULUM BAŞLADI\e[37m'
+pkg install git
+pkg install wget
+pkg install php
+git clone https://github.com/thelinuxchoice/blackeye
+echo -e '\e[32mKURULUM TAMAMLANDI ÇIKIŞ YAPARAK ÇALIŞTIRABİLİRSİNİZ'
+echo -e '\e[35m 10 Saniye Sonra Ana Menüye Yönlendiriliyorsunuz.'
+sleep 5.0
+bash hacktools.sh
+if [[ $secim == 04 || $secim == 4 ]]; then
+echo -e '\e[36m KURULUM BAŞLADI\e[37m'
+pkg install git 
+pkg install clang 
+git clone https://github.com/zanyarjamal/xerxes 
+echo -e '\e[32mKURULUM TAMAMLANDI ÇIKIŞ YAPARAK ÇALIŞTIRABİLİRSİNİZ'
+echo -e '\e[35m 10 Saniye Sonra Ana Menüye Yönlendiriliyorsunuz.'
+sleep 5.0
+bash hacktools.sh
+if [[ $secim == 05 || $secim == 5 ]]; then
+echo -e '\e[36m KURULUM BAŞLADI\e[37m'
+pkg install git
+pkg install python
+pkg install python2
+https://github.com/cyweb/hammer
+echo -e '\e[32mKURULUM TAMAMLANDI ÇIKIŞ YAPARAK ÇALIŞTIRABİLİRSİNİZ'
+echo -e '\e[35m 10 Saniye Sonra Ana Menüye Yönlendiriliyorsunuz.'
+sleep 5.0
+bash hacktools.sh
+if [[ $secim == 06 || $secim == 6 ]]; then
+echo -e '\e[36m KURULUM BAŞLADI\e[37m'
+apt update
+apt upgrade
+pkg install git
+pkg install python2
+git clone https://github.com/ujjawalsaini3/hulk
+echo -e '\e[32mKURULUM TAMAMLANDI ÇIKIŞ YAPARAK ÇALIŞTIRABİLİRSİNİZ'
+echo -e '\e[35m 10 Saniye Sonra Ana Menüye Yönlendiriliyorsunuz.'
+sleep 5.0
+bash hacktools.sh
+if [[ $secim == 07 || $secim == 7 ]]; then
+echo -e '\e[36m KURULUM BAŞLADI\e[37m'
+pkg upgrade
+pkg install git
+pkg install php
+pkg install curl
+pkg install wget
+git clone https://github.com/thelinuxchoice/shellphish.git
+echo -e '\e[32mKURULUM TAMAMLANDI ÇIKIŞ YAPARAK ÇALIŞTIRABİLİRSİNİZ'
+echo -e '\e[35m 10 Saniye Sonra Ana Menüye Yönlendiriliyorsunuz.'
+sleep 5.0
+bash hacktools.sh
+if [[ $secim == 08 || $secim == 8 ]]; then
+echo -e '\e[36m KURULUM BAŞLADI\e[37m'
+apt install update 
+apt install upgrade 
+pkg install python 
+pkg install git 
+pkg install python2 
+git clone https://github.com/Gameye98/Lazymux.git
+echo -e '\e[32mKURULUM TAMAMLANDI ÇIKIŞ YAPARAK ÇALIŞTIRABİLİRSİNİZ'
+echo -e '\e[35m 10 Saniye Sonra Ana Menüye Yönlendiriliyorsunuz.'
+sleep 5.0
+bash hacktools.sh
+if [[ $secim == 09 || $secim == 9 ]]; then
+echo -e '\e[36m KURULUM BAŞLADI\e[37m'
+apt update
+apt upgrade
+pkg install git
+pkg install php
+apt install git
+apt install php
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+echo -e '\e[32mKURULUM TAMAMLANDI ÇIKIŞ YAPARAK ÇALIŞTIRABİLİRSİNİZ'
+echo -e '\e[35m 10 Saniye Sonra Ana Menüye Yönlendiriliyorsunuz.'
+sleep 5.0
+bash hacktools.sh
+if [[ $secim == 10 || $secim == 010 ]]; then
+echo -e '\e[36m KURULUM BAŞLADI\e[37m'
+apt update
+apt upgrade
+pkg install git
+pkg install python
+pkg install python2
+git clone https://github.com/sqlmapproject/sqlmap.git
+echo -e '\e[32mKURULUM TAMAMLANDI ÇIKIŞ YAPARAK ÇALIŞTIRABİLİRSİNİZ'
+echo -e '\e[35m 10 Saniye Sonra Ana Menüye Yönlendiriliyorsunuz.'
+sleep 5.0
+bash hacktools.sh
+
