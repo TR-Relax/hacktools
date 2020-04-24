34cd34a0f9dacc10c5aec1c434207b1e25274df2
TR-Relax <63736122+TR-Relax@users.noreply.github.com>
2020-04-24 10:15:45 +0300

    Add files via upload

---
+++ hacktools.sh
@@ -0,0 +1,139 @@
+clear
+echo -e '\e[32m░█░█░█▀█░█▀▀░█░█░░░▀█▀░█▀█░█▀█░█░░░█▀▀
+░█▀█░█▀█░█░░░█▀▄░░░░█░░█░█░█░█░█░░░▀▀█
+░▀░▀░▀░▀░▀▀▀░▀░▀░░░░▀░░▀▀▀░▀▀▀░▀▀▀░▀▀▀'
+sleep 0.3
+echo -e '\e[35m Türkiyem Siber Tim'
+sleep 0.3
+echo -e '\e[32m Edit By \e[31mRelax'
+sleep 0.3
+echo -e '\e[31m[\e[36m00\e[31m]\e[34m Gereksinimleri İndir'
+sleep 0.3
+echo -e '\e[31m[\e[37m01\e[31m]\e[37m Bomb-Botnet'
+sleep 0.3
+echo -e '\e[31m[\e[37m02\e[31m]\e[37m DDos-Attack'
+sleep 0.3
+echo -e '\e[31m[\e[37m03\e[31m]\e[37m BlackEye'
+sleep 0.3
+echo -e '\e[31m[\e[37m04\e[31m]\e[37m Nexphisher'
+sleep 0.3
+echo -e '\e[31m[\e[37m05\e[31m]\e[37m Hammer'
+sleep 0.3
+echo -e '\e[31m[\e[37m06\e[31m]\e[37m Hulk'
+sleep 0.3
+echo -e '\e[31m[\e[37m07\e[31m]\e[37m Zphisher'
+sleep 0.3
+echo -e '\e[31m[\e[37m08\e[31m]\e[37m Lazymux'
+sleep 0.3
+echo -e '\e[31m[\e[37m09\e[31m]\e[37m RED HAWK'
+sleep 0.3
+echo -e '\e[31m[\e[37m10\e[31m]\e[37m SqlMap'
+sleep 0.4
+read -p $'\e[36mİşlemi Seçiniz\e[35m → \e[37m' secim
+echo -e '\e[31mHATALI GİRİŞ YAPTINIZ\e[37m' 
+if [[ $secim == 00 || $secim == 000 ]]; then		        		
+		  		 
+clear
+echo -e '\e[31mKURULUM BAŞLADI...'
+sleep 0.1
+pkg install clang -y
+pkg install tor -y
+pkh install openssh -y
+pkg install python python2 -y
+pkg install git -y
+pkg install python3 -y
+pkg install php -y
+echo -e '\e[36mKURULUM BİTTİ...'
+sleep 1.1			 
+bash hacktools.sh
+elif [[ $secim == 01 || $secim == 1 ]]; then
+echo -e '\e[36m KURULUM BAŞLADI\e[37m'
+cd
+git clone https://github.com/nishinosukasa/Bomb-Botnet
+echo -e '\e[32mKURULUM TAMAMLANDI ÇIKIŞ YAPARAK ÇALIŞTIRABİLİRSİNİZ'
+echo -e '\e[35m 10 Saniye Sonra Ana Menüye Yönlendiriliyorsunuz.'
+sleep 5.0
+bash hacktools.sh
+
+elif [[ $secim == 02 || $secim == 2 ]]; then
+echo -e '\e[36m KURULUM BAŞLADI\e[37m'
+cd
+git clone https://github.com/Ha3MrX/DDos-Attack
+echo -e '\e[32mKURULUM TAMAMLANDI ÇIKIŞ YAPARAK ÇALIŞTIRABİLİRSİNİZ'
+echo -e '\e[35m 10 Saniye Sonra Ana Menüye Yönlendiriliyorsunuz.'
+sleep 5.0
+bash hacktools.sh
+elif [[ $secim == 03 || $secim == 3 ]]; then
+echo -e '\e[36m KURULUM BAŞLADI\e[37m'
+cd
+git clone https://github.com/thelinuxchoice/blackeye
+echo -e '\e[32mKURULUM TAMAMLANDI ÇIKIŞ YAPARAK ÇALIŞTIRABİLİRSİNİZ'
+echo -e '\e[35m 10 Saniye Sonra Ana Menüye Yönlendiriliyorsunuz.'
+sleep 5.0
+bash hacktools.sh
+elif [[ $secim == 04 || $secim == 4 ]]; then
+echo -e '\e[36m KURULUM BAŞLADI\e[37m'
+cd
+git clone https://github.com/htr-tech/nexphisher
+echo -e '\e[32mKURULUM TAMAMLANDI ÇIKIŞ YAPARAK ÇALIŞTIRABİLİRSİNİZ'
+echo -e '\e[35m 10 Saniye Sonra Ana Menüye Yönlendiriliyorsunuz.'
+sleep 5.0
+bash hacktools.sh
+elif [[ $secim == 05 || $secim == 5 ]]; then
+echo -e '\e[36m KURULUM BAŞLADI\e[37m'
+cd
+git clone https://github.com/cyweb/hammer
+echo -e '\e[32mKURULUM TAMAMLANDI ÇIKIŞ YAPARAK ÇALIŞTIRABİLİRSİNİZ'
+echo -e '\e[35m 10 Saniye Sonra Ana Menüye Yönlendiriliyorsunuz.'
+sleep 5.0
+bash hacktools.sh
+elif [[ $secim == 06 || $secim == 6 ]]; then
+echo -e '\e[36m KURULUM BAŞLADI\e[37m'
+cd
+git clone https://github.com/ujjawalsaini3/hulk
+echo -e '\e[32mKURULUM TAMAMLANDI ÇIKIŞ YAPARAK ÇALIŞTIRABİLİRSİNİZ'
+echo -e '\e[35m 10 Saniye Sonra Ana Menüye Yönlendiriliyorsunuz.'
+sleep 5.0
+bash hacktools.sh
+elif [[ $secim == 07 || $secim == 7 ]]; then
+echo -e '\e[36m KURULUM BAŞLADI\e[37m'
+cd
+git clone https://github.com/htr-tech/zphisher
+echo -e '\e[32mKURULUM TAMAMLANDI ÇIKIŞ YAPARAK ÇALIŞTIRABİLİRSİNİZ'
+echo -e '\e[35m 10 Saniye Sonra Ana Menüye Yönlendiriliyorsunuz.'
+sleep 5.0
+bash hacktools.sh
+elif [[ $secim == 08 || $secim == 8 ]]; then
+echo -e '\e[36m KURULUM BAŞLADI\e[37m'
+cd
+git clone https://github.com/Gameye98/Lazymux.git
+echo -e '\e[32mKURULUM TAMAMLANDI ÇIKIŞ YAPARAK ÇALIŞTIRABİLİRSİNİZ'
+echo -e '\e[35m 10 Saniye Sonra Ana Menüye Yönlendiriliyorsunuz.'
+sleep 5.0
+bash hacktools.sh
+elif [[ $secim == 09 || $secim == 9 ]]; then
+echo -e '\e[36m KURULUM BAŞLADI\e[37m'
+cd
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+echo -e
+echo -e '\e[32mKURULUM TAMAMLANDI ÇIKIŞ YAPARAK ÇALIŞTIRABİLİRSİNİZ'
+echo -e '\e[35m 10 Saniye Sonra Ana Menüye Yönlendiriliyorsunuz.'
+sleep 5.0
+bash hacktools.sh
+elif [[ $secim == 10 || $secim == 010 ]]; then
+echo -e '\e[36m KURULUM BAŞLADI\e[37m'
+cd
+git clone https://github.com/sqlmapproject/sqlmap
+echo -e
+echo -e '\e[32mKURULUM TAMAMLANDI ÇIKIŞ YAPARAK ÇALIŞTIRABİLİRSİNİZ'
+echo -e '\e[35m 10 Saniye Sonra Ana Menüye Yönlendiriliyorsunuz'
+sleep 5.0
+bash hacktools.sh
+									
+else
+	echo -e '\e[31mHATALI GİRİŞ!'
+	sleep 1
+	bash hacktools.sh
+fi
+
+
